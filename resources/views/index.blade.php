0a7e9f632c5c114f0604bc41a4e8cfb98a6702dd
hakim-asrori <saneglos005@gmail.com>
2026-01-16 11:06:14 +0700

    update call extends

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('lopi::layout')
 
 @section('content')
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ea quibusdam tenetur voluptatibus quo in facilis et iure
