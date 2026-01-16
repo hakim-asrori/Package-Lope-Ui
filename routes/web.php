c6943f1b3b12f5c83e10032fb3ce0e6417913693
hakim-asrori <saneglos005@gmail.com>
2026-01-16 15:15:08 +0700

    add route assets

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\{File, Route};
 use HakimAsrori\Lopi\Http\Controllers\DashboardController;
-use Illuminate\Support\Facades\Route;
 
 Route::prefix(config('lopi.path'))
     ->middleware(config('lopi.auth.middlewares'))
@@ -12,3 +12,16 @@ Route::prefix(config('lopi.path'))
         Route::get('/{resource}/create', [DashboardController::class, 'resourceCreate'])->name('lopi.resource.create');
         Route::get('/{resource}/{record}/edit', [DashboardController::class, 'resourceEdit'])->name('lopi.resource.edit');
     });
+
+Route::get('/lopi-assets/{path}', function ($path) {
+    $fullPath = __DIR__ . '/../resources/dist/' . $path;
+
+    if (!File::exists($fullPath)) {
+        abort(404);
+    }
+
+    $file = File::get($fullPath);
+    $type = File::mimeType($fullPath);
+
+    return response($file)->header('Content-Type', $type);
+})->where('path', '.*')->name('lopi.assets');
