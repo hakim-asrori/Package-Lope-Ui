a71aeafd3420f35bece4dfd4e5220fd1890605d7
hakim-asrori <saneglos005@gmail.com>
2026-01-16 15:17:08 +0700

    update path asset

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,8 +10,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-    <link rel="stylesheet" href="{{ route('lopi.assets', 'css/lopi.css') }}">
-    <script src="{{ route('lopi.assets', 'js/lopi.js') }}"></script>
+    <link rel="stylesheet" href="{{ route('lopi.assets', 'lopi.css') }}">
+    <script src="{{ route('lopi.assets', 'lopi.js') }}"></script>
 </head>
 
 <body>
