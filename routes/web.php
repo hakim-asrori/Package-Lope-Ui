e1d266d48357637f6cb8425c140ddd9c9067e880
hakim-asrori <saneglos005@gmail.com>
2026-01-16 10:44:25 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::prefix(config('path'))
     ->middleware(config('auth.middlewares'))
     ->group(function () {
-        Route::get('/', [DashboardController::class, 'index'])->name('my-dashboard.index');
+        Route::get('/', [DashboardController::class, 'index'])->name('lopi.index');
 
-        Route::get('/{resource}', [DashboardController::class, 'resourceIndex'])->name('my-dashboard.resource.index');
-        Route::get('/{resource}/create', [DashboardController::class, 'resourceCreate'])->name('my-dashboard.resource.create');
-        Route::get('/{resource}/{record}/edit', [DashboardController::class, 'resourceEdit'])->name('my-dashboard.resource.edit');
+        Route::get('/{resource}', [DashboardController::class, 'resourceIndex'])->name('lopi.resource.index');
+        Route::get('/{resource}/create', [DashboardController::class, 'resourceCreate'])->name('lopi.resource.create');
+        Route::get('/{resource}/{record}/edit', [DashboardController::class, 'resourceEdit'])->name('lopi.resource.edit');
     });
